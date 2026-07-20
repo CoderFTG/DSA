ebaad1bed0621c339434ebbd895a2ff78e1d7113
Tanishq Gaur <71374687+CoderFTG@users.noreply.github.com>
2026-07-20 17:25:45 +0530

    Time: 4 ms (97.01%) | Memory: 47.2 MB (64.37%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,37 +1,48 @@
 class Solution {
+
+    public void rev(int[] arr, int i, int j) {
+        while (i < j) {
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+            i++;
+            j--;
+        }
+    }
+
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
 
         int m = grid.length;
         int n = grid[0].length;
+        int size = m * n;
 
-        int total = m * n;
+        int[] flat = new int[size];
 
-        k %= total;
+        int idx = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                flat[idx++] = grid[i][j];
+            }
+        }
+
+        k %= size;
+
+        // Right rotate by k
+        rev(flat, 0, size - 1);
+        rev(flat, 0, k - 1);
+        rev(flat, k, size - 1);
 
         List<List<Integer>> ans = new ArrayList<>();
 
+        idx = 0;
         for (int i = 0; i < m; i++) {
             List<Integer> row = new ArrayList<>();
 
             for (int j = 0; j < n; j++) {
-                row.add(0);
+                row.add(flat[idx++]);
             }
 
             ans.add(row);
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                int oldIndex = i * n + j;
-
-                int newIndex = (oldIndex + k) % total;
-
-                int newRow = newIndex / n;
-                int newCol = newIndex % n;
-
-                ans.get(newRow).set(newCol, grid[i][j]);
-            }
         }
 
         return ans;
