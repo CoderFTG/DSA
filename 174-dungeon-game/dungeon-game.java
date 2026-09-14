6f441f5222f50dc495a6fc7b5131068aa181b8f4
Tanishq Gaur <71374687+CoderFTG@users.noreply.github.com>
2026-09-14 21:53:55 +0530

    Time: 5 ms (2.51%) | Memory: 46.9 MB (5.22%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,34 +1,27 @@
 class Solution {
-    int m;
-    int n;
-    int[][] dp = new int[201][201];
-    public int solve(int i, int j, int[][] dungeon) {
-        if(i >= m || j >= n) {
-            return Integer.MAX_VALUE;
-        }
-
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        } 
-
-        if(i == m-1 && j == n-1) {
-            if(dungeon[i][j] > 0) {
-                return 1;
-            }
-            return Math.abs(dungeon[i][j]) + 1;
-        }
-
-        int right = solve(i, j+1, dungeon);
-        int down = solve(i+1, j, dungeon);
-
-        int result = Math.min(right, down) - dungeon[i][j];
-
-        return dp[i][j] = (result > 0) ? result : 1; 
-    }
     public int calculateMinimumHP(int[][] dungeon) {
-        m = dungeon.length;
-        n = dungeon[0].length;
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        
+        int[][] dp = new int[201][201];
         for(int[] cell : dp) Arrays.fill(cell, -1);
-        return solve(0, 0, dungeon);
+
+        for(int i = m-1; i >= 0; i--) {
+            for(int j = n-1; j >= 0; j--) {
+                if(i == m-1 && j == n-1) {
+                    dp[i][j] = (dungeon[i][j] > 0) ? 1 : Math.abs(dungeon[i][j]) + 1;
+                }
+                else {
+                    int right = (j+1 >= n) ? Integer.MAX_VALUE : dp[i][j+1];
+                    int down = (i+1 >= m) ? Integer.MAX_VALUE : dp[i+1][j];
+
+                    int result = Math.min(right, down) - dungeon[i][j];
+
+                    dp[i][j] = (result > 0) ? result : 1;
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 }
